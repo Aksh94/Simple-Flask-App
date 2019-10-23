ac37956378a085c725cc00bf21cac3aecbff4f3c
Akshay Shakya <akshay@cteinternational.net>
2019-10-23 17:38:05 +0530

    Make it better

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
@@ -11,5 +14,6 @@ def hello():
 def hello_name(name):
     return "Hello {}!".format(name)
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
